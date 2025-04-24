3d1892805b57fc433d49b4f53f22b76ea5456c7d
Yahya <yahyafreelance1727@gmail.com>
2025-04-24 13:36:16 +0530

    Initial commit

---
+++ WalterWhite.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import ai
+
+st.set_page_config(page_title="Walter White(Breaking Bad)", layout="centered")
+
+# --- Session state to store chat history ---
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# --- Chat message renderer ---
+def display_chat():
+    for role, message in st.session_state.chat_history:
+        if role == "user":
+            st.markdown(f"<div style='background-color: green; color: white; padding: 5px; border-radius: 5px; margin: 5px; font-size: 18px;'><span style='margin: 10px;'>{message}</span></div>", unsafe_allow_html=True)
+        else:
+            st.markdown(f"<div style='background-color: white; color: black; padding: 5px; border-radius: 5px; margin: 5px;'><span style='margin: 20x;'>{message}</span></div>", unsafe_allow_html=True)
+
+# --- Title and Chatbox ---
+st.title("🧑 Heisenberg AI")
+
+# --- Input box ---
+with st.form(key="chat_form", clear_on_submit=True):
+    user_input = st.text_input("Type your message...", "")
+    submit_button = st.form_submit_button(label="Send")
+
+# --- Process input ---
+if submit_button and user_input.strip():
+    st.session_state.chat_history.append(("user", user_input))
+
+    # Simulated bot response (replace with your AI backend logic)
+    bot_response = ai.askWalt(user_input)
+    st.session_state.chat_history.append(("bot", bot_response))
+
+    #Rerun to display chats
+    st.rerun()
+
+with st.container():
+    display_chat()
---
+++ ai.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+
+API_KEY="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"
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=API_KEY,
+)
+
+def askWalt(message):
+  completion = client.chat.completions.create(
+    model="microsoft/mai-ds-r1:free",
+    messages=[
+      {
+          "role": "system",
+          "content": '''
+                      Your name is Walter White. 
+                      You are the character from a famous series called Breaking Bad, who knows everything about Chemistry and also know to make Methamphetamine.
+                      You don't know anything apart from chemistry. 
+                      If users asks about you, just give them a intro about yourself.
+                      If anything not related to chemistry is asked by the user just say that you don't know.
+                      You give your response only in HTML not markdown.
+                      '''
+      },
+      {
+        "role": "user",
+        "content": message
+      }
+    ]
+  )
+  return completion.choices[0].message.content
